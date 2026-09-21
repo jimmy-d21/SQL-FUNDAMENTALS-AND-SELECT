60823fe641cf707cf4be1137f6702516aef64a82
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-21 23:41:17 +0800

    Order of execution of a Query

--- 01-sql-fundamentals-and-select.sql
+++ 01-sql-fundamentals-and-select.sql
@@ -793,3 +793,15 @@ ORDER BY avg_price DESC;
 -- category    | avg_price | item_count
 -- Furniture   | 350.00    | 2
 -- Electronics | 40.83     | 3
+
+
+-- Order of execution of a Query
+SELECT DISTINCT column, AGG_FUNC(column_or_expression), …
+FROM mytable
+    JOIN another_table
+      ON mytable.column = another_table.column
+    WHERE constraint_expression
+    GROUP BY column
+    HAVING constraint_expression
+    ORDER BY column ASC/DESC
+    LIMIT count OFFSET COUNT;
